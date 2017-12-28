bf275efb1046399c95daeb2072602aa7b2ad70e8
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2017-12-28 02:50:12 -0500

    JModelica scripts added / 3 models added

--- bin/run_fmu.JModelica.py
+++ bin/run_fmu.JModelica.py
@@ -1,7 +1,47 @@
-# Run an FMU model with JModelica
-# Run from an environment set up for JModelica such as jm_python.sh
+#!/usr/bin/env python
 
-import os, sys
+# Runs an FMU with JModelica
+#
+# Project: QSS Solver
+#
+# Language: Python 2.7 and 3.x
+#
+# Developed by Objexx Engineering, Inc. (http://objexx.com) under contract to
+# the National Renewable Energy Laboratory of the U.S. Department of Energy
+#
+# Copyright (c) 2017 Objexx Engineerinc, Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# (1) Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+# (3) Neither the name of the copyright holder nor the names of its
+#     contributors may be used to endorse or promote products derived from this
+#     software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES
+# GOVERNMENT, OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Notes
+#  Run from an environment set up for JModelica such as jm_python.sh
+
+# Imports
+import os, re, sys
 import numpy
 from pyfmi import load_fmu
 
@@ -33,10 +73,13 @@ except:
     pass
 
 # Generate output files
+temp_re = re.compile( 'temp_\d+' )
 t = res[ 'time' ]
 for key in res.keys():
-    if not key[ 0 ] == '_': # Not internal variable
+    if ( key[ 0 ] != '_' ) or ( key[ 0:5 ] == '__zc_' ): # Not internal variable
         if ( key[ 0:4 ] == 'der(' ) and ( key[ -1 ] == ')' ):
             pass # Skip derivatives
+        elif temp_re.match( key ):
+            pass # Skip temporaries
         elif key != 'time':
             numpy.savetxt( key + '.out', numpy.c_[ t, res[ key ] ], fmt = '%-20.16g', delimiter = '\t' )
---
+++ mdl/Achilles/Achilles.mo
@@ -0,0 +1,9 @@
+within ;
+model Achilles
+  Real x1( start = 0);
+  Real x2( start = 2);
+equation
+  der( x2) = -x1;
+  der( x1) = 1.5 * x2 - 0.5 *x1;
+annotation( experiment( StopTime=10, Tolerance=1e-4 ) );
+end Achilles;
