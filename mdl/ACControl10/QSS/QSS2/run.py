9a85f5ede70b0fd6d7b40cec7a561e292737e082
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2022-09-12 01:16:40 -0400

    ACControl10 QSS notes updated

--- mdl/ACControl10/QSS/QSS2/run.py
+++ mdl/ACControl10/QSS/QSS2/run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import subprocess, sys
-args = ' --dtND=1e-4 --dtInf=0.001 --out=sROZDXL ' + ' '.join( sys.argv[1:] )
-#args = ' --rTol=1e-6 --dtND=1e-4 --dtInf=0.001 --dtMax=0.01 ' + ' '.join( sys.argv[1:] ) # Using a tighter tolerance and dtMax gets closer to the correct/CVode simulation
+args = ' --dtND=1e-4 --dtInf=0.001 --out=sROZDX ' + ' '.join( sys.argv[1:] )
 with open( 'run.log', 'w' ) as log:
     subprocess.run( 'run_QSS.py' + args, stdout = log, stderr = subprocess.STDOUT, shell = True )
