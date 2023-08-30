bd8e3c39d0a205b7b3cf3c64bc2c500e761a76e8
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2023-08-29 23:02:11 -0400

    run script updates for latest QSS

--- mdl/Scalable/QSS/6x6/rQSS2_1d/run.py
+++ mdl/Scalable/QSS/6x6/rQSS2_1d/run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import subprocess, sys
-args = ' --relax --rTol=2.5e-6 --bin=U:0.2 --zFac=1000 --dtND=2e-4 --dtInf=0.2 --dtOut=1 --tEnd=86400 --out=sSROZDXQ ' + ' '.join( sys.argv[1:] )
+args = ' --rTol=2.5e-6 --bin=U:0.2 --zFac=1000 --dtND=2e-4 --dtInf=0.2 --dtOut=1 --tEnd=86400 --out=sSROZDXQ ' + ' '.join( sys.argv[1:] )
 with open( 'run.log', 'w' ) as log:
     subprocess.run( 'run_QSS.py' + args, stdout = log, stderr = subprocess.STDOUT, shell = True )
