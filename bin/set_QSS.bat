29e8bad3805974da47641a74edfed6e75dd6ec6d
Stuart Mentzer <Stuart_Mentzer@objexx.com>
2024-01-17 17:47:42 -0500

    set_QSS.bat upd

--- bin/set_QSS.bat
+++ bin/set_QSS.bat
@@ -1,7 +1,5 @@
 @echo off
-rem Set environment for using these scripts with QSS
-rem Customize for your system
-set SOEP_QSS_Test_bin=%~dp0
-set SOEP_QSS_Test_bin=%SOEP_QSS_Test_bin:~0,-1%
-set PATH=%PATH%;%SOEP_QSS_Test_bin%;
-set SOEP_QSS_Test_bin=
+:: Set environment for running QSS
+set PATH=%PATH%;%~dp0
+set OMP_NUM_THREADS=6
+set OMP_WAIT_POLICY=ACTIVE
